6804910bbe5c35d7aa197276a4e3dabf2688394b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-03-16 13:21:20 -0300

    add initial version of the code

---
+++ projects/pokebank-api/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pokebank-api
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ projects/pokebank-api/src/pokebank_api/core.clj
@@ -0,0 +1,8 @@
+(ns pokebank-api.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(foo "oi")
