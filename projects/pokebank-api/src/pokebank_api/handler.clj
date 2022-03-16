8fc3aeaf2ed33eafe2b39f4f9330335d5d326b8b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-03-15 23:01:52 -0300

    update

--- projects/pokebank-api/src/pokebank_api/handler.clj
+++ projects/pokebank-api/src/pokebank_api/handler.clj
@@ -1,10 +1,12 @@
 (ns pokebank-api.handler
   (:require [compojure.core :refer :all]
+            [compojure.handler :refer [site]]
             [compojure.route :as route]
-            [cheshire.core :as json]
-            [environ.core :refer [env]]
+            [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            #_ [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [environ.core :refer [env]]
+            [cheshire.core :as json]))
 
 (defroutes app-routes
   (GET "/" [] {
@@ -13,8 +15,8 @@
   })
   (route/not-found "Not Found"))
 
-;; (def app
-;;   (wrap-defaults app-routes site-defaults))
+#_ (def app
+  (wrap-defaults app-routes site-defaults))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
